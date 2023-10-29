82a55c46992f53b563e8e2fd65b8951bc5eb80a9
N.Nikitins <nikita.nikitins@lightsoft.lv>
2023-10-30 00:42:02 +0200

    App update

--- main.go
+++ main.go
@@ -2,9 +2,32 @@
 
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"errors"
+	"fmt"
+	"net"
+
+	"github.com/gin-gonic/gin"
+)
 
 var router *gin.Engine
+
+func findAvailablePort() (string, error) {
+	// Define the port range you want to check
+	startPort := 8080
+	endPort := 8300
+
+	for port := startPort; port <= endPort; port++ {
+		address := fmt.Sprintf(":%d", port)
+		listener, err := net.Listen("tcp", address)
+		if err == nil {
+			_ = listener.Close()
+			return address, nil
+		}
+	}
+
+	return "", errors.New("No available port in the range")
+}
 
 func main() {
 
@@ -16,6 +39,13 @@ func main() {
 	// Handle GET requests at /article/view/some_article_id
 	router.GET("/article/view/:article_id", getArticle)
 
-	router.Run()
+	port, err := findAvailablePort()
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Found opened port: %v\n", port)
+	router.Run(port)
 
 }
